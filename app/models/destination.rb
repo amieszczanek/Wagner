dd33c8b45a502ac026f4b95ffd42bd770cba14cd
amieszczanek <amieszczanek@sigma.inf.ug.edu.pl>
2011-05-14 17:41:43 +0200

    dodano validacje

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,7 @@
 class Destination < ActiveRecord::Base
   has_many :seats, :dependent => :destroy
+  
+  validates :wyjazd_z, :presence => true
+  validates :przyjazd_do, :presence => true
+  validates :termin, :presence => true
 end
--- app/models/seat.rb
+++ app/models/seat.rb
@@ -2,4 +2,7 @@ class Seat < ActiveRecord::Base
   belongs_to :destination
   
   validates :nazwisko, :length => { :minimum => 4 }
+  validates_numericality_of :telefon
+  validates :nazwisko, :presence => true
+  validates :telefon, :presence => true
 end
